51d13e26ca513ccea8906684e147ac0e0c5bf8e8
Frank Lastres <flastresabreu@gmail.com>
2023-04-09 22:42:56 -0400

    DbContext - Inyeccion de Dependencia

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,160 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+
+        private readonly ILogger<VillaController> _logger;
+        private readonly AplicationDbContext _db;
+
+        public VillaController(ILogger<VillaController> logger, AplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            _logger.LogInformation("Obtener las Villas");
+            return Ok(_db.Villas.ToList());
+        }
+
+        [HttpGet("id:int", Name = "GetVilla")]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id == 0) 
+            {
+                _logger.LogError("Error al traer Villa con Id" + id);
+                return BadRequest();
+            }
+
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+
+            if (villa is null) return NotFound();
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        public ActionResult<VillaDto> CrearVilla([FromBody] VillaDto villaDto)
+        {
+            if (villaDto is null) return BadRequest(villaDto);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            if (_db.Villas.FirstOrDefault(v => v.Nombre.ToLower() == villaDto.Nombre.ToLower()) != null)
+            {
+                ModelState.AddModelError("NombreExiste", "La villa ya existe");
+                return BadRequest(ModelState);
+            }
+
+            Villa modelo = new()
+            {
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _db.Villas.Add(modelo);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("GetVilla", new { id = villaDto.Id }, villaDto);
+        }
+
+        [HttpDelete("id:int")]
+        public IActionResult EliminarVilla(int id) 
+        {
+            if (id == 0) return BadRequest();
+
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+
+            if (villa is null) return NotFound();
+
+            _db.Villas.Remove(villa);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPut("id:int")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto) 
+        {
+            if (villaDto is null || id!=villaDto.Id) return BadRequest();
+
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+            if (villa is null) return NotFound();
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _db.Villas.Update(modelo);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("id:int")]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto is null || id == 0) return BadRequest();
+
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+            if (villa is null) return NotFound();
+
+            VillaDto villaDto = new()
+            {
+                Id = villa.Id,
+                Nombre = villa.Nombre,
+                Detalle = villa.Detalle,
+                ImagenUrl = villa.ImagenUrl,
+                Ocupantes = villa.Ocupantes,
+                Tarifa = villa.Tarifa,
+                MetrosCuadrados = villa.MetrosCuadrados,
+                Amenidad = villa.Amenidad
+            };
+
+            patchDto.ApplyTo(villaDto, ModelState);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            Villa modelo = new()
+            {
+                Id = villaDto.Id,
+                Nombre = villaDto.Nombre,
+                Detalle = villaDto.Detalle,
+                ImagenUrl = villaDto.ImagenUrl,
+                Ocupantes = villaDto.Ocupantes,
+                Tarifa = villaDto.Tarifa,
+                MetrosCuadrados = villaDto.MetrosCuadrados,
+                Amenidad = villaDto.Amenidad
+            };
+
+            _db.Villas.Update(modelo);
+            _db.SaveChanges();
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ MagicVilla_API/Migrations/20230409063055_AlimentarTablaVilla.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVilla_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FechaActualizacion",
+                table: "Villas",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaCreacion",
+                table: "Villas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaActualizacion", "FechaCreacion", "FechaCrecaion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la Villa", new DateTime(2023, 4, 9, 2, 30, 55, 605, DateTimeKind.Local).AddTicks(3919), new DateTime(2023, 4, 9, 2, 30, 55, 605, DateTimeKind.Local).AddTicks(3866), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la Villa", new DateTime(2023, 4, 9, 2, 30, 55, 605, DateTimeKind.Local).AddTicks(3925), new DateTime(2023, 4, 9, 2, 30, 55, 605, DateTimeKind.Local).AddTicks(3923), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", 40, "Premium Vista a la Piscina", 4, 150.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "FechaCreacion",
+                table: "Villas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FechaActualizacion",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
